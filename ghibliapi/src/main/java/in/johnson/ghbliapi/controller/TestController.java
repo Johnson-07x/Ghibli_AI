5cb47aaa136c20be3fe7e069d8776dcd2ed782b1
Johnson A J <johnson.07x@gmail.com>
2026-03-24 01:06:59 +0530

    fix: Fixing Docker Config to deploy

---
+++ ghibliapi/src/main/java/in/johnson/ghbliapi/controller/TestController.java
@@ -0,0 +1,13 @@
+package in.johnson.ghbliapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/check")
+    public String check() {
+        return "API is working!";
+    }
+}
